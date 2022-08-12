65e2dd6e3f15eb93db08a2afd9e4db96ddfde52c
PavanKumar Elango <104477667+Explore-Dream-Discover@users.noreply.github.com>
2022-08-13 01:42:38 +0530

    Update model_config.py

--- backend/models/model_config.py
+++ backend/models/model_config.py
@@ -2,7 +2,7 @@ from logging import exception
 from keras.models import load_model
 import os
 
-model_path = 'backend\models\Cnn_Surface_detection v1.1.h5'
+model_path = 'backend/models/Surface_detection_cnn.h5'
 
 def model_loading():
     calling_cnn = load_model(model_path)
