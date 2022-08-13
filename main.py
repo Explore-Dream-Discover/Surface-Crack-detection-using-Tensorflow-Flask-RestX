8e95269b93089db977eafa84f3be508598ba7c7f
PavanKumar Elango <104477667+Explore-Dream-Discover@users.noreply.github.com>
2022-08-13 20:32:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import os
 app = Flask(__name__)
 api = Api(app, version='1.0',
           title='Surface crack detection ',
-          description='cnn and vgg16')
+          description='Please upload the wall's visual image.')
 
 ns = api.namespace('Note', 
-                   description="Whatever your namespace is Please upload the wall's visual image.")
+                   description="Model constructed using Convolution neural network")
 upload_parser = ns.parser()
 upload_parser.add_argument('file', location='files',
                            type=FileStorage, required=True)
